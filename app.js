9f1b187222cbd82e781cfab5adc5acd39bb09637
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-10-01 10:13:59 -0400

    added button to events div

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 let searchButton = document.querySelector("button");
 let searchField = document.querySelector("input");
 const npsApiKey = "9qWimhDTY1nm3a4cAnyoCDi6KOOIDZZteaBJHCbZ";
-const eventsField = document.querySelector("#events-div");
+const eventsField = document.querySelector("#events-section");
 const siteBody = document.querySelector("body");
+const nextEventButton = document.querySelector("#next-event-button");
+let counter = 0;
 
+
+//This function is what you use to populate all the events on the event piece of the page. you 
+//call using the secondResponnse variable from the axios call.
 const events = (obj) => {
   let dataArr = obj.data.data;
 
@@ -18,37 +23,36 @@ const events = (obj) => {
     let timeEnd = element.times[0].timeend;
     let title = element.title;
     let typeOfEvent = element.types[0];
-    datesStart = element.date;
-    dateEnd = element.dateend;
-    category = element.category;
-    contactName = element.contactname;
-    contactTelephoneNumber = element.contacttelephonenumber;
-    timeStart = element.times[0].timestart;
-    timeEnd = element.times[0].timeend;
-    title = element.title;
-    typeOfEvent = element.types[0];
+    let location = element.location;
     eventList.innerHTML = `<p> Start date: ${datesStart}
   <br>End date: ${dateEnd}<br>It will be a: ${category}
   <br>Your contact for this event is: ${contactName}
   <br>The contact's phone number is: ${contactTelephoneNumber}<br>
   Event will start at: ${timeStart}<br> Event will end at: ${timeEnd}<br>
-  The event is: ${title}<br> It is a: ${typeOfEvent}</p>`;
+  The event is: ${title}<br> It is a: ${typeOfEvent}<br> Location: ${location}</p>`;
+
     eventsField.append(eventList);
   });
-  console.log(dataArr);
+  // console.log(dataArr);
+}
+
+
+//This function loads all of the events as soon as the page loads
+siteBody.onload = async () => {
+  const secondResponse = await axios.get(`https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/events?api_key=${npsApiKey}&stateCode=NY&limit=10`);
+  let individualEvents = events(secondResponse);
+  // let events = secondResponse.data.data;
+  // let eventDiv = document.createElement('div');
+  nextEventButton.addEventListener("click", () => {
+    let currentDiv = individualEvents[counter];
+    eventsField.append(currentDiv);
+    counter++;
+  })
 }
 
 const getData = async () => {
   // const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/parks?api_key=${npsApiKey}&fields=images&statecode=MI`);
-  // const secondResponse = await axios.get(`https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/events?api_key=${npsApiKey}&stateCode=NY&limit=10`);
-  console.log(secondResponse)
+  console.log(response)
 }
 
-siteBody.onload = async () => {
-  const secondResponse = await axios.get(`https://cors-anywhere.herokuapp.com/https://developer.nps.gov/api/v1/events?api_key=${npsApiKey}&stateCode=NY&limit=10`);
-  events(secondResponse);
-}
-
-searchButton.addEventListener("click", getData)
-
-//save array to a variable... create currentEvent variable and set to variable[0]...set inner
+// searchButton.addEventListener("click", getData)
